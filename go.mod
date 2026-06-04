233228dbd4e12ec0c0024b394efbebecb1ebb8e5
absdekty <...@gmail.com>
2026-06-04 22:10:57 +0300

    Реализован общий мигратор БД
     - Поддерживает разные виды БД
     - Сейчас может поддерживать: SQLite3

--- go.mod
+++ go.mod
@@ -4,9 +4,16 @@ go 1.20
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/golang-jwt/jwt/v4 v4.5.2
+	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.44
+)
 
 require (
-	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
-	github.com/google/uuid v1.6.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.16.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 )
---
+++ pkg/migrator/migrator.go
@@ -0,0 +1,46 @@
+package migrator
+
+import (
+	"database/sql"
+	"embed"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+)
+
+type Driver string
+
+const (
+	SQLite3 Driver = "sqlite3"
+)
+
+func Migrate(db *sql.DB, driver Driver, fs embed.FS, dir string) error {
+	source, err := iofs.New(fs, dir)
+	if err != nil {
+		return fmt.Errorf("failed to create source: %w", err)
+	}
+
+	switch driver {
+	case SQLite3:
+		databaseDriver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+		if err != nil {
+			return fmt.Errorf("failed to create database driver: %w", err)
+		}
+
+		m, err := migrate.NewWithInstance("iofs", source, string(driver), databaseDriver)
+		if err != nil {
+			return fmt.Errorf("failed to create migrator: %w", err)
+		}
+		defer m.Close()
+
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			return fmt.Errorf("failed to apply migrations: %w", err)
+		}
+	default:
+		return fmt.Errorf("unsupported driver: %s", driver)
+	}
+
+	return nil
+}
